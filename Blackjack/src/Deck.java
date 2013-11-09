8c3fc86db25ab07f0613209a77805214d84d91a5
mrmikemcguire <mcguire@mullenhigh.com>
2013-11-09 14:50:50 -0700

    Skeleton finished

--- Blackjack/src/Deck.java
+++ Blackjack/src/Deck.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Deck
 	{
 	static String currentCard;
 	static int currentCardValue;
+	static ArrayList<String> deck = new ArrayList<String>();
 
-	static String[] deck = {"ace of diamonds", "king of diamonds", "queen of diamonds", "jack of diamonds", "ten of diamonds", "nine of diamonds", "eight of diamonds", "seven of diamonds",
+	static String[] listOfCards = {"ace of diamonds", "king of diamonds", "queen of diamonds", "jack of diamonds", "ten of diamonds", "nine of diamonds", "eight of diamonds", "seven of diamonds",
 			"six of diamonds", "five of diamonds", "four of diamonds", "three of diamonds", "two of diamonds", "ace of spades",
 			"king of spades", "queen of spades", "jack of spades", "ten of spades", "nine of spades", "eight of spades", "seven of spades", "six of spades",
 			"five of spades", "four of spades", "three of spades", "two of spades", "ace of clubs", "king of clubs",
@@ -14,28 +17,22 @@ public class Deck
 			"king of hearts", "queen of hearts", "jack of hearts", "ten of hearts", "nine of hearts", "eight of hearts", "seven of hearts", "six of hearts",
 			"five of hearts", "four of hearts", "three of hearts", "two of hearts"};
 	
-//	public static void shuffle()
-//		{
-//        for (int i=0; i<100; i++)
-//        	{
-//            index_1 = generator.nextInt( cards.size() - 1 );
-//            index_2 = generator.nextInt( cards.size() - 1 );
-//
-//            temp = (Card) cards.get( index_2 );
-//            cards.set( index_2 , cards.get( index_1 ) );
-//            cards.set( index_1, temp );
-//        	}
-//		}
-//    public Card drawFromDeck()
-//    	{     
-//        return cards.remove( 0 );
-//    	}
-
-
+	public static void populateDeck()
+		{
+		for(int i=0;i<52;i++)
+			{
+			deck.add(listOfCards[i]);
+			}
+		}
+	
+	public static void shuffle()
+		{
+		Collections.shuffle(deck);	
+		}
+	
 public static void deal()
 		{
-		int randomCardDraw = (int) ((Math.random() * 51) + 1);
-		Deck.currentCard = deck[randomCardDraw];
+		currentCard = deck.get(0);
 		
 	if ((currentCard.substring(0,3).equals ("kin")) || (currentCard.substring(0,3).equals ("que")) || 
 		(currentCard.substring(0,3).equals ("jac")) || (currentCard.substring(0,3).equals ("ten")))
@@ -74,9 +71,11 @@ public static void deal()
 		{
 		currentCardValue = 2;
 		}
-	else
+	if ((currentCard.substring(0,3).equals ("ace")))
 		{
-//		chooseAceValue();
+		currentCardValue = 11;
 		}
+	
+	deck.remove(0);
 	}
 }
